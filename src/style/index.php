7e9b118f7a3652e4f256599f7a6a4432c611fc79
Marc LEPAGNEUL <marc@lepagneul.fr>
2024-02-22 23:20:46 +0000

    First PHP file

---
+++ src/style/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Template</title>
+</head>
+<body>
+    <h1>Hello World !</h1>
+    <p>Ceci est un exemple de page PHP.</p>
+    <?php
+    // Exemple de code PHP
+    $date = date('d-m-Y H:i:s');
+    echo "<p>La date et l'heure actuelles sont : $date</p>";
+    ?>
+</body>
+</html>
